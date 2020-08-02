11a1282e72d94e0fe7f3788ab4ab8d2ef3faf057
Ray <railzy00@gmail.com>
2020-08-03 00:05:52 +0800

    go

--- diy-part2.sh
+++ diy-part2.sh
@@ -10,5 +10,13 @@
 # Description: OpenWrt DIY script part 2 (After Update feeds)
 #
 
+#安装华工路由器插件
+git clone https://github.com/scutclient/scutclient.git
+cp -R ./scutclient/openwrt ./package/scutclient
+git clone https://github.com/scutclient/luci-app-scutclient.git ./feeds/luci/applications/luci-app-scutclient
+./scripts/feeds install -a -p luci
+
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.50.5/g' package/base-files/files/bin/config_generate
+sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+sed -i 's/0.openwrt.pool.ntp.org/ntp.aliyun.com/g' package/base-files/files/bin/config_generate
